dd0d5c8b65f91fdf4aa77dd80f748fd13a8d495c
DarioAlessioR <100212928+DarioAlessioR@users.noreply.github.com>
2022-11-30 17:27:35 -0400

    Add Home component

---
+++ src/components/Home/index.js
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom";
+
+const Home = () => {
+
+
+  return (
+    <div className="container home-page">
+      <div className="text-zone">
+        <h1 className="title">Hi, <br /> I'm Dario Alessio,<br /> web developer.</h1>
+        <h2 className="subtitle">Frontend developer / Ruby on Rails & JavaScript enthusiast.</h2>
+        <Link to="/contact" className="flat-button">CONTACT ME</Link>
+      </div>
+    </div>
+  );
+
+
+}
+
+export default Home
